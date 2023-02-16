de82a0a8f7f4df91f4ebae7247d4265ea450e755
Yanters <yanter@wp.pl>
2023-02-16 22:34:53 +0100

    Dashboard Properties

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { useList } from '@pankod/refine-core';
 import { Box, Stack, Typography } from '@pankod/refine-mui';
-import { PieChart, PropertyReferrals, TotalRevenue } from 'components';
+import {
+  PieChart,
+  PropertyCard,
+  PropertyReferrals,
+  TotalRevenue,
+} from 'components';
 
 const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: 'properties',
+    config: {
+      pagination: {
+        pageSize: 4,
+      },
+    },
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Box>Loading...</Box>;
+  if (isError) return <Box>Error</Box>;
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color='#11142D'>
@@ -44,6 +63,39 @@ const Home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+      <Box
+        flex={1}
+        borderRadius='10px'
+        padding='20px'
+        bgcolor='#fcfcfc'
+        display='flex'
+        flexDirection='column'
+        minWidth='100%'
+        mt='25px'
+      >
+        <Typography fontSize={20} fontWeight={700} color='#11142D'>
+          Latest Properties
+        </Typography>
+        <Box
+          mt='20px'
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '20px',
+          }}
+        >
+          {latestProperties.map((property) => (
+            <PropertyCard
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              price={property.price}
+              location={property.location}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
