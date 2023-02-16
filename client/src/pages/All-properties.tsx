cc2673774ba7559ae85fb20569171812ba87c846
Yanters <yanter@wp.pl>
2023-02-16 17:38:52 +0100

    Properties new layout

--- client/src/pages/All-properties.tsx
+++ client/src/pages/All-properties.tsx
@@ -68,9 +68,23 @@ const AllProperties = () => {
         }}
       >
         <Stack direction='column' width='100%'>
+
+          <Stack
+            direction='row'
+            justifyContent='space-between'
+            alignItems='center'
+          >
           <Typography fontSize={24} fontWeight={700} color='#11142d'>
             {!allProperties.length ? 'No Properties Found' : 'All Properties'}
           </Typography>
+            <CustomButton
+              title='Add Property'
+              handleClick={() => navigate('/properties/create')}
+              backgroundColor='#475be8'
+              color='#fcfcfc'
+              icon={<Add />}
+            />
+          </Stack>
           <Box
             mb={2}
             mt={3}
@@ -152,15 +166,6 @@ const AllProperties = () => {
         </Stack>
       </Box>
 
-      <Stack direction='row' justifyContent='space-between' alignItems='center'>
-        <CustomButton
-          title='Add Property'
-          handleClick={() => navigate('/properties/create')}
-          backgroundColor='#475be8'
-          color='#fcfcfc'
-          icon={<Add />}
-        />
-      </Stack>
       <Box mt='20px' sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }}>
         {allProperties.map((property) => (
           <PropertyCard
